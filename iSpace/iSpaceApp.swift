681ca1f53c01ea8341ef9378b9f785bfb650d238
Victor Castro <victorcastro.io@icloud.com>
2021-11-08 21:08:18 -0500

    Initial Commit

---
+++ iSpace/iSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpaceApp.swift
+//  iSpace
+//
+//  Created by VCASTROC on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct iSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
